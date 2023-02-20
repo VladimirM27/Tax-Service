7312036e2fc1b4347121d486352a156213d72528
volodymyrm27 <vovam6655@gmail.com>
2023-02-20 20:29:39 +0200

    Full refactoring of the DAOFactory.java

--- src/main/java/com/motrechko/taxservice/dao/ConnectionPool.java
+++
@@ -1,7 +0,0 @@
-package com.motrechko.taxservice.dao;
-
-import java.sql.Connection;
-
-public interface ConnectionPool {
-    Connection getConnection(boolean autocommit);
-}
--- src/main/java/com/motrechko/taxservice/dao/DAOFactory.java
+++ src/main/java/com/motrechko/taxservice/dao/DAOFactory.java
@@ -1,41 +1,75 @@
 package com.motrechko.taxservice.dao;
 
-public abstract class DAOFactory {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * A factory for obtaining DAO instances for different entities.
+ */
+public class DAOFactory {
+
+    private static final Logger logger = LogManager.getLogger(DAOFactory.class);
+
     private static DAOFactory instance;
-    protected static ConnectionPool connectionPool;
-    public static synchronized DAOFactory getInstance(){
-        if(instance == null){
-            Class<?> clazz = null;
-            try {
-                clazz = Class.forName(DAOFactory.daoFactoryFCN);
-                instance = (DAOFactory) clazz.getDeclaredConstructor().newInstance();
-                instance.setPooledConnection();
-            } catch (Exception e){
-                //Logger;
-            }
+
+    private final Connection connection;
+
+    /**
+     * Private constructor to ensure that instances are only created via the getInstance() method.
+     * Initializes a new database connection using the ConnectionFactory class.
+     *
+     * @throws SQLException if a database access error occurs
+     */
+    private DAOFactory() throws SQLException {
+        connection = ConnectionFactory.getConnection();
+        logger.info("Created new DAOFactory instance");
+    }
+
+    /**
+     * Returns the singleton instance of the DAOFactory class.
+     * If an instance has not been created yet, creates a new one.
+     *
+     * @return the singleton instance of DAOFactory
+     * @throws SQLException if a database access error occurs
+     */
+    public static synchronized DAOFactory getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DAOFactory();
         }
         return instance;
     }
 
-    private static String daoFactoryFCN;
-
-    public static void setDaoFactoryFCN(String daoFactoryFCN){
-        instance = null;
-        DAOFactory.daoFactoryFCN = daoFactoryFCN;
+    /**
+     * Returns a new UserDAO instance that uses the existing database connection.
+     *
+     * @return a new UserDAO instance
+     */
+    public UserDAO getUserDAO() {
+        logger.debug("Getting UserDAO instance");
+        return new UserDAOImpl(connection);
     }
 
-    public abstract void setPooledConnection();
-
-    private  ConnectionPool getConnectionPool(){
-        if(instance != null) return connectionPool;
-        else {
-            //log
-            throw new IllegalArgumentException("Cannot get ConnectionPool");
-        }
+    /**
+     * Returns a new AdminDAO instance that uses the existing database connection.
+     *
+     * @return a new AdminDAO instance
+     */
+    public AdminDAO getAdminDAO() {
+        logger.debug("Getting AdminDAO instance");
+        return new AdminDAOImpl(connection);
     }
 
-    public abstract ReportDAO getReportDAO();
-    public abstract AdminDAO getAdminDAO();
-    public abstract UserDAO getUserDAO();
-    public abstract void close(AutoCloseable resource);
+    /**
+     * Returns a new ReportDAO instance that uses the existing database connection.
+     *
+     * @return a new ReportDAO instance
+     */
+    public ReportDAO getReportDAO() {
+        logger.debug("Getting ReportDAO instance");
+        return new ReportDAOImpl(connection);
+    }
+
 }

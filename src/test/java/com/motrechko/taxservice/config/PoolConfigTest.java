baa2bcffc406c24e7b588a2d069e951a9ff1f441
volodymyrm27 <vovam6655@gmail.com>
2023-03-09 19:46:08 +0200

    Change PoolConfigTest.java Added testing with mockito

--- src/test/java/com/motrechko/taxservice/config/PoolConfigTest.java
+++ src/test/java/com/motrechko/taxservice/config/PoolConfigTest.java
@@ -1,32 +1,51 @@
 package com.motrechko.taxservice.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 
 public class PoolConfigTest {
-    private DatabaseConfig dbConfig;
-
+    /**
+     A mock DatabaseConfig object used for testing.
+     */
+    @Mock
+    private DatabaseConfig mockDbConfig;
+    /**
+     The PoolConfig object being tested.
+     */
+    private PoolConfig poolConfig;
+    /**
+     Sets up the test environment before each test by creating a mock DatabaseConfig object and initializing the PoolConfig object with it.
+     */
     @BeforeEach
-    public void setup(){
-        dbConfig = new DatabaseConfig();
+    public void setUp()  {
+        MockitoAnnotations.openMocks(this);
+        when(mockDbConfig.getUrl()).thenReturn("jdbc:mysql://localhost:3306/mydb");
+        when(mockDbConfig.getDriver()).thenReturn("com.mysql.jdbc.Driver");
+        when(mockDbConfig.getUsername()).thenReturn("root");
+        when(mockDbConfig.getPassword()).thenReturn("password");
+        poolConfig = new PoolConfig(mockDbConfig);
     }
-
+    /**
+     Tests the getDataSource() method of the PoolConfig class by asserting that the returned BasicDataSource object has the expected properties.
+     */
     @Test
     public void testGetDataSource() {
-        PoolConfig poolConfig = new PoolConfig(dbConfig);
         BasicDataSource dataSource = poolConfig.getDataSource();
-
-        Assertions.assertNotNull(dataSource);
-        Assertions.assertEquals(dbConfig.getUrl(), dataSource.getUrl());
-        Assertions.assertEquals(dbConfig.getDriver(), dataSource.getDriverClassName());
-        Assertions.assertEquals(dbConfig.getUsername(), dataSource.getUsername());
-        Assertions.assertEquals(dbConfig.getPassword(), dataSource.getPassword());
-        Assertions.assertEquals(5, dataSource.getInitialSize());
-        Assertions.assertEquals(10, dataSource.getMaxTotal());
-        Assertions.assertEquals(5, dataSource.getMaxIdle());
-        Assertions.assertEquals(2, dataSource.getMinIdle());
-        Assertions.assertEquals(10000, dataSource.getMaxWaitMillis());
+        assertEquals("jdbc:mysql://localhost:3306/mydb", dataSource.getUrl());
+        assertEquals("com.mysql.jdbc.Driver", dataSource.getDriverClassName());
+        assertEquals("root", dataSource.getUsername());
+        assertEquals("password", dataSource.getPassword());
+        assertEquals(5, dataSource.getInitialSize());
+        assertEquals(10, dataSource.getMaxTotal());
+        assertEquals(5, dataSource.getMaxIdle());
+        assertEquals(2, dataSource.getMinIdle());
+        assertEquals(10000, dataSource.getMaxWaitMillis());
     }
 }
--- src/test/java/com/motrechko/taxservice/dao/DAOFactoryTest.java
+++ src/test/java/com/motrechko/taxservice/dao/DAOFactoryTest.java
@@ -43,4 +43,6 @@ public class DAOFactoryTest {
         assertNotNull(adminDAO);
     }
 
+
+
 }
